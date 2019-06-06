21b5826a04ab7afa37fe8e3c6e6c65cdf73c9b8c
linjj93 <linjj93@hotmail.com>
2019-06-06 12:16:07 +0800

    use async await for fetch

--- app.js
+++ app.js
@@ -9,28 +9,48 @@ function Fruit(props) {
 class Form extends React.Component {
   constructor(props) {
     super(props);
-
+    this.url =
+      "https://my-json-server.typicode.com/thoughtworks-jumpstart/api/fruits";
     this.state = {
       input: "",
       fruits: []
     };
   }
 
-  componentDidMount() {
-    fetch(
-      "https://my-json-server.typicode.com/thoughtworks-jumpstart/api/fruits"
-    )
-      .then(res => res.json())
-      .then(data => {
-        // do something with the data
-        data.map((item, index) => {
-          item["id"] = item.type;
-        });
+  //using promises - non-blocking
+  // componentDidMount() {
+  //   fetch(this.url)
+  //     .then(res => {
+  //       if (res.status >= 400) {
+  //         throw new Error("Failed to fetch"); //abort immediately, because even error reponse is also considered valid response //!response.ok
+  //         // return res.json();
+  //       }
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       // do something with the data
+  //       console.log(data);
+  //       this.setState({
+  //         fruits: data
+  //       });
+  //     })
+  //     .catch(err => console.log(err.message));
+  // }
 
-        this.setState({
-          fruits: data
-        });
+  //async await-blocking
+  async componentDidMount() {
+    try {
+      const res = await fetch(this.url);
+      if (!res.ok) {
+        throw new Error("Something happened");
+      }
+      const data = await res.json();
+      this.setState({
+        fruits: data
       });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   handleChange = () => {
